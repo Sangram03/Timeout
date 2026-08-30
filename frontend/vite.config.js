919c5e710937a12f7bcae22192556512d541c558
Sangram Das <sangramraju143@gmail.com>
2026-08-30 18:26:26 +0530

    just updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.BACKEND_URL || "http://localhost:5001",
+        target: process.env.BACKEND_URL || "http://localhost:3000",
         changeOrigin: true,
       },
     },
