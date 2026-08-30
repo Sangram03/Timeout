c8c6e493b6ef19307e0216b27b933478c174cd61
Sangram Das <sangramraju143@gmail.com>
2026-08-30 20:49:04 +0530

    latest

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ import {
 } from "./middlewares/rateLimiters.js"
  
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5176", "https://timeout-plum-seven.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5176", "https://timeout-seven.vercel.app"],
     credentials: true
 }))  
 app.use(express.json())
